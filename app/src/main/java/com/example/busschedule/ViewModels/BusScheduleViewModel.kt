f4a34bd275c13533f59451d3b2b573051ca61238
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-06-22 12:17:37 +0530

    created view model

---
+++ app/src/main/java/com/example/busschedule/ViewModels/BusScheduleViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.busschedule.ViewModels
+
+import androidx.lifecycle.ViewModel
+import com.example.busschedule.Database.Schedule
+import com.example.busschedule.Database.ScheduleDao
+
+class BusScheduleViewModel(private val scheduleDao: ScheduleDao): ViewModel() {
+    fun fullScedule() : List<Schedule> = scheduleDao.getAll()
+
+    fun scheduleForStop(name : String) : List<Schedule> = scheduleDao.getByStopName(name)
+}
