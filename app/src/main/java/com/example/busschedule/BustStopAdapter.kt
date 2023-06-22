2431c5f52db65b68a36b73d30cb367eddecdad64
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-06-22 18:26:54 +0530

    Created list adapter viewholder

--- app/src/main/java/com/example/busschedule/BustStopAdapter.kt
+++ app/src/main/java/com/example/busschedule/BustStopAdapter.kt
@@ -1,13 +1,20 @@
 package com.example.busschedule
 
+import android.annotation.SuppressLint
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.busschedule.Database.Schedule
 import com.example.busschedule.databinding.BusStopItemBinding
+import java.text.SimpleDateFormat
 
-class BustStopAdapter(private val onItemClicked : (Schedule)-> Unit) : ListAdapter<Schedule,BustStopAdapter.BusStopViewHolder>() {
+class BustStopAdapter(private val onItemClicked : (Schedule)-> Unit) : ListAdapter<Schedule,BustStopAdapter.BusStopViewHolder>(DiffCallback) {
+
     class BusStopViewHolder(private var binding: BusStopItemBinding):RecyclerView.ViewHolder(binding.root) {
-
+        @SuppressLint("SimpleDateFormat")
+        fun bind(schedule: Schedule){
+            binding.stopNameTextView.text = schedule.stopName
+            binding.arrivalTimeTextView.text = SimpleDateFormat("h:mm a").format(schedule.arrivalTime.toLong()*1000)
+        }
     }
 
 
