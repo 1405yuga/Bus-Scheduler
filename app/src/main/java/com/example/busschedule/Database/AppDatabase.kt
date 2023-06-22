6088af27ef582863737c95b7d3554cb14823b665
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-06-22 17:42:52 +0530

    App Database instance created

--- app/src/main/java/com/example/busschedule/Database/AppDatabase.kt
+++ app/src/main/java/com/example/busschedule/Database/AppDatabase.kt
@@ -1,22 +1,24 @@
 package com.example.busschedule.Database
 
 import android.content.Context
-import android.provider.CalendarContract.Instances
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
+@Database(entities = arrayOf(Schedule::class), version = 1)
 abstract class AppDatabase() : RoomDatabase() {
-    abstract fun scheduleDao() : ScheduleDao
+    abstract fun scheduleDao(): ScheduleDao
 
-    companion object{
+    companion object {
         @Volatile
-        private var INSTANCE : AppDatabase? = null
+        private var INSTANCE: AppDatabase? = null
 
-        fun getDatabase(context : Context) : AppDatabase{
+        fun getDatabase(context: Context): AppDatabase {
             return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(context, AppDatabase::class.java, "app_database")
-                    .createFromAsset("database/bus_schedule.db")
-                    .build()
+                val instance =
+                    Room.databaseBuilder(context, AppDatabase::class.java, "app_database")
+                        .createFromAsset("database/bus_schedule.db")
+                        .build()
                 INSTANCE = instance
 
                 instance
