40fb06320bd163515058b6e91054004f4f7cde0e
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-06-24 10:59:17 +0530

    flow feature in viewmodel

--- app/src/main/java/com/example/busschedule/ViewModels/BusScheduleViewModel.kt
+++ app/src/main/java/com/example/busschedule/ViewModels/BusScheduleViewModel.kt
@@ -3,10 +3,11 @@ package com.example.busschedule.ViewModels
 import androidx.lifecycle.ViewModel
 import com.example.busschedule.Database.Schedule
 import com.example.busschedule.Database.ScheduleDao
+import kotlinx.coroutines.flow.Flow
 
 class BusScheduleViewModel(private val scheduleDao: ScheduleDao) : ViewModel() {
-    fun fullScedule(): List<Schedule> = scheduleDao.getAll()
+    fun fullScedule(): Flow<List<Schedule>> = scheduleDao.getAll()
 
-    fun scheduleForStop(name: String): List<Schedule> = scheduleDao.getByStopName(name)
+    fun scheduleForStop(name: String): Flow<List<Schedule>> = scheduleDao.getByStopName(name)
 
 }
